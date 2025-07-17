d0aad76143083090cefba4865d3217eb3f113a69
ibrahim-cn <ibrahimm.can@hotmail.com>
2025-07-17 11:27:26 +0300

    commit

---
+++ main/java/com/ibrahimcan/springboot/SPringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahimcan.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SPringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SPringbootApplication.class, args);
+    }
+
+}
