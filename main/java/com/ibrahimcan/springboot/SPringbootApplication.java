88c6a2263ecb9d1037b5e2dd830d73608d1dc4b7
ibrahim-cn <ibrahimm.can@hotmail.com>
2025-07-17 13:38:17 +0300

    commit1

--- main/java/com/ibrahimcan/springboot/SPringbootApplication.java
+++ main/java/com/ibrahimcan/springboot/SPringbootApplication.java
@@ -9,5 +9,36 @@ public class SPringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SPringbootApplication.class, args);
     }
+//            arrays.method();
+//            this.
+//            super
+//            break and continiue
+//            optional
+//            cast
+//            primitive
+//            wrapperö classs
+//            random
+//            math
+//            %d %s %f
+//            final ,
+//            static
+//            decoder encoder
+//            instanceof//
+//   String methods
+//      enums
+    //solid
+    //for each
+    //kiss structure - DRY
+    //MONAT st
+//    interface-intheritance - abstract
+    //clas
+    // Bean yapısı
+    //  POJO
+    // jar var
+    // io
+    //consturctor
+    //exeption
+    //optional
+
 
 }
