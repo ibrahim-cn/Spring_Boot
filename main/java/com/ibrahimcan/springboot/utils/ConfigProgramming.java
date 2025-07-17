0bc7cdc9f0fef9459397ae71040a6ff7bf05fa43
ibrahim-cn <ibrahimm.can@hotmail.com>
2025-07-17 12:32:54 +0300

    commit1

---
+++ main/java/com/ibrahimcan/springboot/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.ibrahimcan.springboot.utils;
+
+public class ConfigProgramming {
+    //DevTools ==> LiveReload
+    //file ==> settings ==> compiler ==> Build project automatically (Ctrl+Alt+S)
+//    file ==> Advanced Settings ==>Allow Autmake to start even (OK clicked)
+}
---
+++ main/java/com/ibrahimcan/springboot/utils/ideaShortcut.java
@@ -0,0 +1,10 @@
+package com.ibrahimcan.springboot.utils;
+
+public class ideaShortcut {
+//    Setting ==> ctrl+alt+s
+//    Alt+Ins ==> constructer, toString,getter setter
+//    Ctrl+/ => single comment
+//    Ctrl+Shift+/ => single comment
+
+
+}
